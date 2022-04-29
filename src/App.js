b0fb135a1956e9c516ac67557879275f3321bfbc
Jacob Akerson <jacob.akerson@csus.edu>
2022-04-29 09:56:41 -0700

    added test component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Test from './Test'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Sac State Class Schedule App</h1>
+        <Test test="Test" />
       </header>
     </div>
   );
