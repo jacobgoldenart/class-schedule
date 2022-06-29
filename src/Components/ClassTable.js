b550c9376527d9041e9b2ebdd49ece1d4f11f5a0
Jacob Akerson <jacob.akerson@csus.edu>
2022-06-29 08:40:26 -0700

    added anchor links to individual courses.

--- src/Components/ClassTable.js
+++ src/Components/ClassTable.js
@@ -8,29 +8,28 @@ const ExpandedComponent = (section) => {
     return (
         //conditional rendering to avoid displaying if the atribute is missing
         <div key={uuid()} className='class-info-exp'>
-            <p onClick={() => console.log(section)}> Jora </p>
-            {section.subject_code && <p><span>Subject Code</span>: {section.subject_code}</p>}
-            {section.catalog_number && <p>Catalog Number: {section.catalog_number}</p>}
-            {section.class_section && <p>Class Section: {section.class_section}</p>}
-            {section.course_id && <p>Course ID: {section.course_id}</p>}
-            {section.class_title && <p>Class title: {section.class_title}</p>}
-            {section.units && <p>Units: {section.units}</p>}
-            {section.class_number && <p>Class Number: {section.class_number}</p>}
-            {section.component && <p>Component: {section.component}</p>}
-            {section.seats_total && <p>Seats Total: {section.seats_total}</p>}
-            {section.seats_available && <p>Seats Available: {section.seats_available}</p>}
-            {section.instructor && <p>Instructor: {section.instructor}</p>}
-            {section.building && <p>Building: {section.building}</p>}
-            {section.room && <p>Room: {section.room}</p>}
-            {section.days && <p>Days: {section.days}</p>}
-            {section.start_time && <p>Start Time: {section.start_time}</p>}
-            {section.end_time && <p>End Time: {section.end_time}</p>}
-            {section.meeting_number && <p>Meeting Number: {section.meeting_number}</p>}
-            {section.session_code && <p>Session Code: {section.session_code}</p>}
-            {section.ge_grad_req && <p>GE Grad Req: {section.ge_grad_req}</p>}
-            {section.class_meeting_start_date && <p>Class Meeting Start Date: {section.class_meeting_start_date}</p>}
-            {section.class_meeting_end_date && <p>Class Meeting End Date: {section.class_meeting_end_date}</p>}
-            {section.class_ldesc && <p>Class Description: {section.class_ldesc}</p>}
+           {section.data.subject_code && <p><span>Subject Code</span>: {section.data.subject_code}</p>}
+            {section.data.catalog_number && <p>Catalog Number: {section.data.catalog_number}</p>}
+            {section.data.class_section && <p>Class Section: {section.data.class_section}</p>}
+            {section.data.course_id && <p>Course ID: {section.data.course_id}</p>}
+            {section.data.class_title && <p>Class title: {section.data.class_title}</p>}
+            {section.data.units && <p>Units: {section.data.units}</p>}
+            {section.data.class_number && <p>Class Number: {section.data.class_number}</p>}
+            {section.data.component && <p>Component: {section.data.component}</p>}
+            {section.data.seats_total && <p>Seats Total: {section.data.seats_total}</p>}
+            {section.data.seats_available && <p>Seats Available: {section.data.seats_available}</p>}
+            {section.data.instructor && <p>Instructor: {section.data.instructor}</p>}
+            {section.data.building && <p>Building: {section.data.building}</p>}
+            {section.data.room && <p>Room: {section.data.room}</p>}
+            {section.data.days && <p>Days: {section.data.days}</p>}
+            {section.data.start_time && <p>Start Time: {section.data.start_time}</p>}
+            {section.data.end_time && <p>End Time: {section.data.end_time}</p>}
+            {section.data.meeting_number && <p>Meeting Number: {section.data.meeting_number}</p>}
+            {section.data.session_code && <p>Session Code: {section.data.session_code}</p>}
+            {section.data.ge_grad_req && <p>GE Grad Req: {section.data.ge_grad_req}</p>}
+            {section.data.class_meeting_start_date && <p>Class Meeting Start Date: {section.data.class_meeting_start_date}</p>}
+            {section.data.class_meeting_end_date && <p>Class Meeting End Date: {section.data.class_meeting_end_date}</p>}
+            {section.data.class_ldesc && <p>Class Description: {section.data.class_ldesc}</p>}
         </div>
     )
 }
@@ -103,7 +102,7 @@ export default function ClassTable({ section }) {
                     fixedHeader
                     compact
                     selectableRowsHighlight
-                    expandableRowsComponent={() => ExpandedComponent(rows[1])}
+                    expandableRowsComponent={(e) => ExpandedComponent(e,rows)}
                 />
             </div>
         </>
